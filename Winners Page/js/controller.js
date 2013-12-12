2314c6dcb896fc980b0e8bb17f1fd29b25a20c4a
Charles Weems <charlie.weems@gmail.com>
2013-12-12 12:17:51 -0500

    Final Cleanup

--- Winners Page/js/controller.js
+++
@@ -1,6 +0,0 @@
-function mainCtrl($scope, $http){
-	$http.get('content/items.json').success(function(data) {
-    $scope.items = data; //Loads content items
-    $scope.Filter = {name : 'Camco'}; //Sets filter for first section
-  });
-}
